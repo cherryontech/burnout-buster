e9c36b9bfd72713be6972313cff8d6e29235d030
manu vasconcelos <manu.vasconcelos@gmail.com>
2023-03-12 15:50:12 -0300

    remove navigation from header

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,7 @@ import styles from '@/styles/Header.module.css'
 function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/" className={styles.tag}>Home</Link>
       <div className={styles.logo}>Burnout Buster</div>
-      <Link href="/" className={styles.tag}>Exit</Link>
     </header>
   )
 }
