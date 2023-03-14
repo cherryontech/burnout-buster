2ebceb370fa61b7472c0ef077b09f4ca9dd415d3
Ida <idaeilashi@gmail.com>
2023-03-13 22:18:30 -0700

    Feat/23 Create quiz page

---
+++ src/components/content.js
@@ -0,0 +1,41 @@
+/* File contains both questions and semantic scale answers */
+
+const Content = [
+    {
+    question: "I feel exhausted, overwhelmed, and emotionally drained. I can\'t seem to find the motivation to keep going.",
+    quizAnswers: [
+        {answer: 1},
+        {answer: 2},
+        {answer: 3},
+        {answer: 4},
+        {answer: 5},
+        {answer: 6},
+        {answer: 7}
+    ]},
+    {
+    question: "Affording mental health support can be challenging right now.",
+    quizAnswers: [
+        {answer: 1},
+        {answer: 2},
+        {answer: 3},
+        {answer: 4},
+        {answer: 5},
+        {answer: 6},
+        {answer: 7}
+        ]},
+    {
+    question: "I am feeling overwhelmed and anxious about what the future holds, due to my recent job loss.",
+    quizAnswers: [
+        {answer: 1},
+        {answer: 2},
+        {answer: 3},
+        {answer: 4},
+        {answer: 5},
+        {answer: 6},
+        {answer: 7}
+    ]}
+]
+
+
+export default Content;
+
