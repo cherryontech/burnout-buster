882186fae38b165e3b5ccc6d2ddeab0dd2d4c48e
manu vasconcelos <manu.vasconcelos@gmail.com>
2023-03-14 20:53:44 -0300

    remove the layout when it's the index page
    We don't have the header in the index/landing page,
    so it's the only page that shouldn't inherit(?) the layout.
    I followed this suggestion
    
    https://stackoverflow.com/a/66914975/18739367
    
    However, I'm not sure if this is the best way to go about this.
    Leaving it like this for now.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,12 @@ import '@/styles/globals.css'
 import Layout from '../components/layout'
 import '@/styles/fonts.css'
 
-export default function App({ Component, pageProps }) {
+
+export default function App({ Component, pageProps, ...appProps }) {
+
+  if ([`/`].includes(appProps.router.pathname))
+  return <Component {...pageProps} />;
+
   return (
     <Layout>
       <Component {...pageProps} />
