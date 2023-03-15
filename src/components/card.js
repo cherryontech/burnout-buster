465f880dfb7c582f8c7227b57a768b92c8f0b0e7
manu vasconcelos <manu.vasconcelos@gmail.com>
2023-03-14 22:04:15 -0300

    update structure and style

--- src/components/card.js
+++ src/components/card.js
@@ -4,10 +4,6 @@ import styles from '@/styles/Results.module.css'
 function Card() {
   return (
     <div className={styles.card}>
-      <div className='card_tittle'>
-        <h2>Sensa.health</h2>
-      </div>
-
       <picture className={styles.card_image}>
         <source srcSet="https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp" type="image/webp" />
         <img
@@ -18,15 +14,20 @@ function Card() {
         />
       </picture>
 
-      <div className={styles.card_footer}>
-        <div className='card_buttons'>
-          <button className={styles.card_button}></button>
-          <button className={styles.card_button}></button>
+      <section className={styles.card_text}>
+        <div className={styles.card_title}>
+          <h2>Sensa.health</h2>
+          <p className={styles.tag}>Mental health support</p>
         </div>
-        <div className={styles.tag}>
-          <Link href="https://sensa.health/" className={styles.card_resource_link}>Learn more</Link>
+
+        <div>
+          <p>CBT (Cognitive Behavioral Therapy) content to calming exercises, and even guides on self-improvement your journey to a stress-free life has never been quite so simple.</p>
         </div>
-      </div>
+
+        <div className={styles.card_redirect}>
+          <Link href="https://sensa.health/" className={[styles.card_resource_link, styles.tag].join(" ")}>Learn more</Link>
+        </div>
+      </section>
     </div>
   )
 }
--- src/components/tag.js
+++
@@ -1,9 +0,0 @@
-import styles from '@/styles/Results.module.css'
-
-function Tag({tagName}) {
-  return (
-    <li className={styles.tag}>{tagName}</li>
-  )
-}
-
-export default Tag
