1beeaa782f7a5033f72e63befde05cc4b30eca1e
manu vasconcelos <manu.vasconcelos@gmail.com>
2023-03-16 21:43:32 -0300

    import results data
    Instead of keeping the data inside the results page, we now are
    importing it from a separate file. For some reason it hadn't worked
    before, my guess, I wrote something wrong because I was sleepy.

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,49 +1,7 @@
 import Head from 'next/head'
 import styles from '@/styles/Results.module.css'
 import Card from '../components/card.js'
-
-const resultsObject2 = {
-  sensa:  {
-    image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
-    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
-    title: "Sensa.Health",
-    tag: "Mental health support",
-    url: "https://sensa.health/",
-    description: "CBT (Cognitive Behavioral Therapy) content to calming exercises, and even guides on self-improvement your journey to a stress-free life has never been quite so simple."
-  },
-  pocketwell:  {
-    image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
-    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
-    title: "Pocketwell",
-    tag: "Mental health support",
-    url: "https://play.google.com/store/apps/details?id=com.greenspacehealth.wtc&gl=US",
-    description: "The Wellness Together Canada program offers a range of services, including online self-assessments, self-guided courses, group coaching, and one-on-one counseling with trained professionals. The program also includes a crisis support line for individuals experiencing a mental health emergency."
-  },
-  sidebyside:  {
-    image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
-    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
-    title: "Side by Side",
-    tag: "Online community",
-    url: "https://www.mind.org.uk/information-support/side-by-side-our-online-community/",
-    description: "Side by Side is an online community where you can listen, share and be heard. Provides advice and support to empower anyone experiencing a mental health problem."
-  },
-  mighty:  {
-    image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
-    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
-    title: "The Mighty",
-    tag: "Online community",
-    url: "https://themighty.com/",
-    description: "The Mighty also offers a community feature where individuals can connect with others who share their experiences, ask questions, and provide support to one another. The platform covers a wide range of topics, including chronic illnesses, mental health, and disabilities, and aims to create awareness and reduce stigma surrounding these issues."
-  },
-  jobnetwork:  {
-    image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
-    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
-    title: "Jobnetwork",
-    tag: "Job search",
-    url: "community.thejobnetwork.com/",
-    description: "This website offers job listings, career advice, and resources to help you with your job search. They also have a section dedicated to managing stress and burnout while job searching."
-  }
-}
+import { resultsObject } from '@/data/results.js'
 
 export default function Results() {
   return (
@@ -62,7 +20,7 @@ export default function Results() {
         </section>
 
         <section className={styles.cards_container}>
-          {Object.entries(resultsObject2).map(itemArr => (
+          {Object.entries(resultsObject).map(itemArr => (
             <Card
               key={itemArr[0]}
               imgUrl={Object.values(itemArr[1].image)}
