66a97bfbba0b620edd2d650730f52fa0b1db1166
manu vasconcelos <manu.vasconcelos@gmail.com>
2023-03-16 21:35:56 -0300

    add external url and image to card
    Since this is an external image we should use <a> and not <Link>
    But for whatever reason I kept getting a broken url when passing it
    as a prop to the <a> and <source> tags, using .join('') fixed it.
    But I have no idea why it came all broke apart.
    
    We should dive deeper into this, but we can look at it after the
    presentation this sunday.

--- src/pages/results.js
+++ src/pages/results.js
@@ -5,6 +5,7 @@ import Card from '../components/card.js'
 const resultsObject2 = {
   sensa:  {
     image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
+    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
     title: "Sensa.Health",
     tag: "Mental health support",
     url: "https://sensa.health/",
@@ -12,30 +13,34 @@ const resultsObject2 = {
   },
   pocketwell:  {
     image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
+    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
     title: "Pocketwell",
     tag: "Mental health support",
-    url: "https://sensa.health/",
+    url: "https://play.google.com/store/apps/details?id=com.greenspacehealth.wtc&gl=US",
     description: "The Wellness Together Canada program offers a range of services, including online self-assessments, self-guided courses, group coaching, and one-on-one counseling with trained professionals. The program also includes a crisis support line for individuals experiencing a mental health emergency."
   },
   sidebyside:  {
     image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
+    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
     title: "Side by Side",
     tag: "Online community",
-    url: "https://sensa.health/",
+    url: "https://www.mind.org.uk/information-support/side-by-side-our-online-community/",
     description: "Side by Side is an online community where you can listen, share and be heard. Provides advice and support to empower anyone experiencing a mental health problem."
   },
   mighty:  {
     image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
+    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
     title: "The Mighty",
     tag: "Online community",
-    url: "https://sensa.health/",
+    url: "https://themighty.com/",
     description: "The Mighty also offers a community feature where individuals can connect with others who share their experiences, ask questions, and provide support to one another. The platform covers a wide range of topics, including chronic illnesses, mental health, and disabilities, and aims to create awareness and reduce stigma surrounding these issues."
   },
   jobnetwork:  {
     image: "https://sensa.health/static/06abdded9fa8e6e3e192540f099f1218/4b995/bean_in_heart.webp",
+    alt: "Antropomorphic cartoon bean relaxing inside a cartoon heart",
     title: "Jobnetwork",
     tag: "Job search",
-    url: "https://sensa.health/",
+    url: "community.thejobnetwork.com/",
     description: "This website offers job listings, career advice, and resources to help you with your job search. They also have a section dedicated to managing stress and burnout while job searching."
   }
 }
@@ -61,9 +66,10 @@ export default function Results() {
             <Card
               key={itemArr[0]}
               imgUrl={Object.values(itemArr[1].image)}
+              imgAlt={Object.values(itemArr[1].alt)}
               cardTitle={Object.values(itemArr[1].title)}
               cardTag={Object.values(itemArr[1].tag)}
-              linkUrl = {Object.values(itemArr[1].url)}
+              cardUrl = {Object.values(itemArr[1].url)}
               cardDesc = {Object.values(itemArr[1].description)}
             />
           ))}
