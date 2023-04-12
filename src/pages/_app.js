ea15bdf955ad41de42d042d004d6a8b88a8e5a45
Ida <idaeilashi@gmail.com>
2023-04-12 16:26:57 -0700

    updated app file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,22 @@
 import '@/styles/globals.css'
 import Layout from '../components/layout'
 import '@/styles/fonts.css'
+import {useState} from 'react';
 
 
 export default function App({ Component, pageProps, ...appProps }) {
+
+  const [formData, setFormData] = useState({});
+  const updateFormData = (newData) => {
+    setFormData({ ...formData, ...newData });
+  };
 
   if ([`/`].includes(appProps.router.pathname))
   return <Component {...pageProps} />;
 
   return (
     <Layout>
-      <Component {...pageProps} />
+      <Component {...pageProps} updateFormData = {updateFormData}/>
     </Layout>
   )
 }
